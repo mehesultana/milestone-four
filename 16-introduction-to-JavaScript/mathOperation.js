78dffa942d0ebbd9a2e690cfa00e35245504ff8d
mehesultana <sultanamehe01@gmail.com>
2021-07-31 03:55:56 +0600

    math operation

---
+++ 16-introduction-to-JavaScript/mathOperation.js
@@ -0,0 +1,11 @@
+//16-6 (advanced) Mathematical operation shorthand
+var price1 = 31;
+var price2 = 10;
+var sum = price1 + price2;
+var difference = price2 - price1;
+var multiplication = price1 * price2;
+var division = price1 / price2;
+console.log(sum);
+console.log(difference);
+console.log(multiplication);
+console.log(division);
