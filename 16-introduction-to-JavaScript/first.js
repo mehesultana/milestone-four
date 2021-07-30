0b9c3d0bb05a1d43a21c83980c8c164584cd6950
mehesultana <sultanamehe01@gmail.com>
2021-07-31 02:16:13 +0600

    comment add

--- 16-introduction-to-JavaScript/first.js
+++ 16-introduction-to-JavaScript/first.js
@@ -1,1 +1,3 @@
+// 16-1 Introduction to JavaScript, Run JavaScript in VSCode
+
 console.log(3);
--- 16-introduction-to-JavaScript/variable.js
+++ 16-introduction-to-JavaScript/variable.js
@@ -1,7 +1,21 @@
+// 16-2 What is variable, five things you need to declare a variable
+
+// 16-3 Variable type, Numeric, String, Boolean
+
+// Variable type- Numeric
+
 var price = 21;
 var age = 13;
 var temperature = 37;
 var member = 5;
 var height = 60;
 
+// Variable type- String
+
 var name = 'mehe sultana';
+var school = 'police line high school';
+
+// Variable type- Boolean
+
+var pass = true;
+var subscribed = false;
