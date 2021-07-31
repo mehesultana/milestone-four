6d28987a80f9c043970b9d8824c2b1e0d6601f5a
mehesultana <sultanamehe01@gmail.com>
2021-08-01 00:41:32 +0600

    output

--- 17-fundamental-concepts-array&contionals/array.js
+++ 17-fundamental-concepts-array&contionals/array.js
@@ -3,6 +3,15 @@ var friendsAge = [11, 14, 15];
 console.log(friendsAge);
 
 var muriCanacurFee = [20, 15, 18, 225];
+console.log(muriCanacurFee);
+
 var nayikas = ['mahi', 'opu', 'sabana'];
+console.log(nayikas);
+
 var oddNumbers = [1, 3, 5, 7, 9];
+console.log(oddNumbers);
+
 var vowels = ['a', 'e', 'i', 'o', 'u'];
+console.log(vowels);
+var vowelsCount = vowels.length;
+console.log(vowelsCount);
