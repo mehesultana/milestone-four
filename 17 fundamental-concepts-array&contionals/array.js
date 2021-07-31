471028352388275de75b4826f8f38c3c8f66f58e
mehesultana <sultanamehe01@gmail.com>
2021-08-01 00:24:13 +0600

    array

---
+++ 17 fundamental-concepts-array&contionals/array.js
@@ -0,0 +1,6 @@
+// 17-2 Declare Array, array length and array index
+var friendsAge = [11, 14, 15];
+var muriCanacurFee = [20, 15, 18, 225];
+var nayikas = ['mahi', 'opu', 'sabana'];
+var oddNumbers = [1, 3, 5, 7, 9];
+var vowels = ['a', 'e', 'i', 'o', 'u'];
