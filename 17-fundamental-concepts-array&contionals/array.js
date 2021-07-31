ca9d4da80fe8488983baf7169f2404b97cec80f4
mehesultana <sultanamehe01@gmail.com>
2021-08-01 00:28:18 +0600

    rename

--- 17-fundamental-concepts-array&contionals/array.js
+++ 17-fundamental-concepts-array&contionals/array.js
@@ -1,5 +1,7 @@
 // 17-2 Declare Array, array length and array index
 var friendsAge = [11, 14, 15];
+console.log(friendsAge);
+
 var muriCanacurFee = [20, 15, 18, 225];
 var nayikas = ['mahi', 'opu', 'sabana'];
 var oddNumbers = [1, 3, 5, 7, 9];
