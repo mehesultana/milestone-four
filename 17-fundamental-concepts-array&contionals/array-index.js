2aeab6ead2611fbd933116e8707e297d79f78e2f
mehesultana <sultanamehe01@gmail.com>
2021-08-01 01:07:30 +0600

    array index

---
+++ 17-fundamental-concepts-array&contionals/array-index.js
@@ -0,0 +1,24 @@
+// 17-3 Array index, get and set by index, indexOf
+
+// set index
+var books = ['hablu', 'gablu', 'bablu', 'alu', 'potol'];
+var babluIndex = books.indexOf('bablu');
+console.log(babluIndex);
+
+var numbers = [73, 53, 81, 92, 45];
+var index = numbers.indexOf(53);
+console.log(index);
+
+console.log(numbers);
+numbers[1] = 64; //replace
+console.log(numbers);
+
+//get index
+var secondIndex = books[2];
+console.log(secondIndex);
+
+var secondIndex = books[4];
+console.log(secondIndex);
+
+var secondIndex = books[0];
+console.log(secondIndex);
