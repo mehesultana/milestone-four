1b57892e18079117bd5f56eed817016699e1436e
mehesultana <sultanamehe01@gmail.com>
2021-08-01 00:03:37 +0600

    intro

---
+++ 17 fundamental-concepts-array&contionals/variable.js
@@ -0,0 +1,11 @@
+// 17-1 Module Introduction and Variable recap
+
+//number
+var bookPrice = 120;
+bookPrice = 125;
+
+//string
+var studentName = 'Sogir Uddin';
+
+//boolean
+var isExpensive = true;
