f499003ad4497f48fbc0b3dc136d8d6afae6977b
mehesultana <sultanamehe01@gmail.com>
2021-07-31 16:41:36 +0600

    different types conversion

---
+++ 16-introduction-to-JavaScript/conversion.js
@@ -0,0 +1,25 @@
+// 16-7 (advanced) Integer float parseInt parseFloat type conversion
+
+//string add
+var firstName = 'Mehe';
+var lastName = 'Sultana';
+var fullName = firstName + ' ' + lastName; //' ' => empty string for gap
+console.log(fullName);
+
+var first = 'Ariana';
+var last = 'Grande';
+var name = first + ' ' + last;
+console.log(name);
+
+//integer
+var book = 3;
+//float
+var price = 85.98;
+
+var onionPrice = '42.77';
+var eggPrice = '41.33';
+var onionPriceNumber = parseInt(onionPrice);
+console.log(onionPriceNumber);
+
+var eggPriceNumber = parseFloat(eggPrice);
+console.log(eggPriceNumber);
