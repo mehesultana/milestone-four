8969d61971c089b5cfe3c03f30c26bb46fe2f446
mehesultana <sultanamehe01@gmail.com>
2021-08-01 02:33:54 +0600

    comparisn

---
+++ 17-fundamental-concepts-array&contionals/comparison.js
@@ -0,0 +1,17 @@
+// 17-5 Compare variables and Comparison operator
+console.log(6 > 8);
+console.log(6 < 8);
+console.log(6 == 8);
+console.log(6 == 6);
+console.log(5 != 6);
+console.log(6 != 6);
+
+var myLove = 99;
+var yourLove = 100;
+console.log(myLove > yourLove);
+console.log(myLove == yourLove);
+console.log(myLove < yourLove);
+console.log(myLove != yourLove);
+
+console.log(studyGood && goodPerson);
+console.log(isBanker || goodResult);
