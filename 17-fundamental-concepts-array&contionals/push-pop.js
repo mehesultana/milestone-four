1dc96d41d54162350bf8353e1c372e8526da88e2
mehesultana <sultanamehe01@gmail.com>
2021-08-01 01:44:15 +0600

    push pop

---
+++ 17-fundamental-concepts-array&contionals/push-pop.js
@@ -0,0 +1,10 @@
+// 17-4 Add or remove element from array using push, pop
+var lastbag = ['alu', 'potol', 'mula'];
+lastbag.push('kocu');
+lastbag.push('begun');
+lastbag.push('fulkopi');
+console.log(lastbag);
+
+var friendsAge = [11, 14, 15, 12];
+friendsAge.push('13');
+console.log(friendsAge);
