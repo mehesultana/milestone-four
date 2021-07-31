4fbc9e4d99bcff8c698ab3b4429ff476c54831a9
mehesultana <sultanamehe01@gmail.com>
2021-07-31 16:00:09 +0600

    shorthand

--- 16-introduction-to-JavaScript/mathOperation.js
+++ 16-introduction-to-JavaScript/mathOperation.js
@@ -1,4 +1,5 @@
 //16-6 (advanced) Mathematical operation shorthand
+
 var price1 = 31;
 var price2 = 10;
 var sum = price1 + price2;
@@ -6,7 +7,30 @@ var difference = price2 - price1;
 var multiplication = price1 * price2;
 var division = price1 / price2;
 
-console.log('add', sum);
-console.log(difference);
-console.log('gun korsi', multiplication);
-console.log(division);
+// console.log('add', sum);
+// console.log(difference);
+// console.log('gun korsi', multiplication);
+// console.log(division);
+
+// Mathematical operation shorthand
+var doublePrice = price2 * 20;
+var newPrice = price1 + 10;
+price1 = price1 + 10;
+price1 += 44;
+price2 = price2 - 2;
+price2 -= 2;
+console.log(newPrice);
+console.log(price1);
+console.log(price2);
+//
+var age = 14;
+age = age + 1;
+age += 1;
+age++;
+console.log(age);
+//
+var love = 100;
+love = love - 1;
+love -= 1;
+love--;
+console.log(love);
