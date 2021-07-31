0fa7d6111e8637944fbfd8d83f1e9d86941eb958
mehesultana <sultanamehe01@gmail.com>
2021-08-01 03:10:14 +0600

    conditional

---
+++ 17-fundamental-concepts-array&contionals/conditional.js
@@ -0,0 +1,38 @@
+// 17-6 Make conditional decision, if-else, comparison
+var isFoodReady = true;
+//if food is ready i will eat
+//if food is ready == true
+// if (condition) {
+
+// }
+
+if (isFoodReady == true) {
+    console.log('maaaaa I will eat now.');
+}
+
+var iphonePrice = 70000;
+var myBudget = 109500;
+
+if (iphonePrice < myBudget) {
+    console.log('iphone');
+}
+
+if (iphonePrice > myBudget) {
+    console.log('shaomi');
+}
+
+// if (condition) {
+//     //
+// }
+// else {
+//     //
+// }
+
+var chickenPrice = 180;
+var myMoney = 850;
+
+if (chickenPrice < myMoney) {
+    console.log('yeahhh! murgir ran khabo');
+} else {
+    console.log('smashed potato with Lentil soup');
+}
