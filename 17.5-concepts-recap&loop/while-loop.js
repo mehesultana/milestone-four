0ca7cb2b92d0c09246ff58aef63af1a5b9b9d939
mehesultana <sultanamehe01@gmail.com>
2021-08-02 01:28:41 +0600

    while loop

--- 17.5-concepts-recap&loop/while-loop.js
+++ 17.5-concepts-recap&loop/while-loop.js
@@ -4,8 +4,8 @@
 var roastGiven = 0;
 while (roastGiven < 7) {
     console.log('Roast den please!!');
-    // roastGiven = roastGiven + 1;
-    // roastGiven += 1;
+    roastGiven = roastGiven + 1;
+    roastGiven += 1;
     roastGiven++;
     console.log(roastGiven);
 }
