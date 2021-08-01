5f3007c5c1ac99759652ebc3b526f14d4ba40e63
mehesultana <sultanamehe01@gmail.com>
2021-08-02 01:02:11 +0600

    while loop

---
+++ 17.5-concepts-recap&loop/while-loop.js
@@ -0,0 +1,11 @@
+//17_5-2 While loop, debug and understand while loop
+
+//While loop
+var roastGiven = 0;
+while (roastGiven < 7) {
+    console.log('Roast den please!!');
+    // roastGiven = roastGiven + 1;
+    // roastGiven += 1;
+    roastGiven++;
+    console.log(roastGiven);
+}
