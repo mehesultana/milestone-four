040dffda6e982b4abfb1ffc3b2525a2231e00ff3
mehesultana <sultanamehe01@gmail.com>
2021-08-01 16:57:58 +0600

    nasted condition

---
+++ 17-fundamental-concepts-array&contionals/nasted-condition.js
@@ -0,0 +1,25 @@
+// 17-8 (Advanced) Multi stage condition and nested conditions
+var danishPrice = 50;
+var butterBreadPrice = 25;
+var toastBiscuitPrice = 10;
+var myBudget = 20;
+var packedWell = false;
+
+if (danishPrice < myBudget) {
+    console.log('Danish kheye danish jaboooooooo');
+} else if (butterBreadPrice < myBudget) {
+    console.log('butterbon diye cha khaboooo');
+} else if (toastBiscuitPrice < myBudget) {
+    console.log('butterbon diye cha khaboooo');
+} else {
+    console.log('batash diye cha khabo karon amr tk nai');
+}
+
+//
+if (danishPrice < myBudget) {
+    if (packedWell == true) {
+        console.log('danish khaboooo');
+    } else {
+        console.log('nah khabo naaaa');
+    }
+}
