c88a15b756b12ce7f8d287e23fb8fa285ffd69ec
mehesultana <sultanamehe01@gmail.com>
2021-08-02 01:34:54 +0600

    array loop

--- 17.5-concepts-recap&loop/array-loop.js
+++ 17.5-concepts-recap&loop/array-loop.js
@@ -1,5 +1,16 @@
 // 17_5-5 Recap loop, run a loop for each element of an array
 
+// basic while loop
+var i = 1;
+while (i <= 7) {
+    console.log(i);
+    i++;
+}
+//basic for loop
+for (var i = 1; i <= 7; i++) {
+    console.log(i);
+}
+
 var numbers = [45, 86, 34, 87, 12, 98, 82, 33, 44];
 for (var i = 0; i < numbers.length; i++) {
     var element = numbers[i];
