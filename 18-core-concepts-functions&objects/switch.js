5b62a28525453b543cc0f2791cb5b74373a30cdb
mehesultana <sultanamehe01@gmail.com>
2021-08-04 14:23:35 +0600

    switch

---
+++ 18-core-concepts-functions&objects/switch.js
@@ -0,0 +1,38 @@
+//18-7 (optional) Javascript switch case break and default
+var color = 'yellow';
+
+if (color == 'blue') {
+    console.log('color is blue');
+} else if (color == 'red') {
+    console.log('color is red');
+} else if (color == 'green') {
+    console.log('color is green');
+} else if (color == 'white') {
+    console.log('color is white');
+} else if (color == 'yellow') {
+    console.log('color is yellow');
+} else {
+    console.log('color is red');
+}
+
+switch (color) {
+    case 'blue':
+        console.log('color is blue');
+        break;
+    case 'red':
+        console.log('color is red');
+        break;
+    case 'white':
+        console.log('color is white');
+        break;
+
+    case 'green':
+        console.log('color is green');
+        break;
+    case 'yellow':
+        console.log('color is yellow');
+        break;
+    case 'black':
+        console.log('color is black');
+        break;
+}
