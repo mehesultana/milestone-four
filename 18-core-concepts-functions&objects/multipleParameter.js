c26542756e48a6d53d4eb3218a9e77bb0230c5df
mehesultana <sultanamehe01@gmail.com>
2021-08-04 14:21:35 +0600

    multiple parameter

---
+++ 18-core-concepts-functions&objects/multipleParameter.js
@@ -0,0 +1,23 @@
+// 18-4 (advanced) Multiple parameter add, multiplication, etc
+function addTwoNumber(number1, number2) {
+    console.log(number1, number2);
+    var total = number1 + number2;
+    return total;
+}
+//add
+var firstNumber = 21;
+var secondNumber = 69;
+var result = addTwoNumber(firstNumber, secondNumber);
+console.log('result value: ', result);
+
+//multiplication
+function multiplyTwonumbers(num1, num2) {
+    var result = num1 * num2;
+    return result;
+}
+
+var total = multiplyTwonumbers(5, 100);
+console.log('total after multiplication', total);
+
+//
+//
