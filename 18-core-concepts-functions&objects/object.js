f08b6a283f1f1ae8fb85d056d6decf18a606b047
mehesultana <sultanamehe01@gmail.com>
2021-08-04 14:16:23 +0600

    object

---
+++ 18-core-concepts-functions&objects/object.js
@@ -0,0 +1,23 @@
+//18-5 Declare multiple objects with multiple properties
+
+//declare multiple object
+var student1 = {
+    id: 135,
+    name: 'Mehe',
+    marks: 87,
+    phone: 09358934,
+};
+
+var mobile = {
+    color: 'gray',
+    price: 15000,
+    storage: '64gb',
+};
+
+var book = {
+    title: 'Start with why',
+    author: 'Simon Sinek',
+    price: 200,
+    pages: 246,
+    publisher: 'penguin',
+};
