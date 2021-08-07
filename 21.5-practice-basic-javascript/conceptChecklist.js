66591704ef39e57ba8bd95ab3bc261e23511a186
mehesultana <sultanamehe01@gmail.com>
2021-08-07 23:41:07 +0600

    concepts checklist

---
+++ 21.5-practice-basic-javascript/conceptChecklist.js
@@ -0,0 +1,23 @@
+//21_5-1 Basic Javascript concept checklist and practice
+
+// 1. write 3 variables (number, string, boolean)
+//number
+var bookPage = 400;
+console.log('bookPage = ', 435);
+
+//string
+var bookName = 'Treasure Island';
+console.log('book name is', bookName);
+
+//boolean
+var isInnocent = true;
+console.log(isInnocent);
+
+// 2. 2 variables using (let, const)
+const myName = 'Mehe';
+console.log(myName);
+
+let number = 35;
+console.log(number);
+
+// 3. Simple Math Operations() +, -, *, /, %)
