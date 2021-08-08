16d31b8c6559f4c2090b4801e08b28641130b39d
mehesultana <sultanamehe01@gmail.com>
2021-08-08 16:28:51 +0600

    Problem solving checklist

---
+++ 21.5-practice-basic-javascript/problemSolvingChecklist.js
@@ -0,0 +1,10 @@
+// 21_5-2 Simple Problem solving checklist and practice
+// 1. conversation (feetToInch)
+var feetToInch = 14;
+var inch = feetToInch * 12;
+console.log(inch);
+
+// 2. conversion (centimeterToMeter)
+var centimeterToMeter = 160;
+var meter = centimeterToMeter / 100;
+console.log(meter);
