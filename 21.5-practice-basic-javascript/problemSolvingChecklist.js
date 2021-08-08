54494fa225596512fc27efd43c093a8411260d4d
mehesultana <sultanamehe01@gmail.com>
2021-08-08 17:40:32 +0600

    Problem solving checklist

--- 21.5-practice-basic-javascript/problemSolvingChecklist.js
+++ 21.5-practice-basic-javascript/problemSolvingChecklist.js
@@ -8,3 +8,22 @@ console.log(inch);
 var centimeterToMeter = 160;
 var meter = centimeterToMeter / 100;
 console.log(meter);
+
+// 3. calculation
+
+function bookCalculator(bookQuantity1, bookQuantity2, bookQuantity3) {
+    const perBookPage1 = 100;
+    const perBookPage2 = 200;
+    const perBookPage3 = 300;
+    //wood calculation
+    const bookPageQuantity1 = bookQuantity1 * perBookPage1;
+    const bookPageQuantity2 = bookQuantity2 * perBookPage2;
+    const bookPageQuantity3 = bookQuantity3 * perBookPage3;
+
+    //total
+    const totalWood = bookPageQuantity1 + bookPageQuantity2 + bookPageQuantity3;
+    return totalWood;
+}
+
+const pageCalculation = bookCalculator(5, 7, 10);
+console.log('page Calculation = ', pageCalculation);
