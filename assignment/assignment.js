828fef023568eb4412a5577ace5708b23ab0fc25
mehesultana <sultanamehe01@gmail.com>
2021-08-09 01:00:04 +0600

    calculation

--- assignment/assignment.js
+++ assignment/assignment.js
@@ -1,5 +1,6 @@
-// 1. conversation (seerToMon)
+//22-1 Basic JavaScript Problem Solving assignment
 
+// 1. conversation (seerToMon)
 function seerToMon(seer) {
     if (typeof seer !== 'number' || seer === 'undefined' || seer === 'null') {
         return 'enter a valid number';
@@ -13,3 +14,21 @@ function seerToMon(seer) {
 
 const mon = seerToMon(160);
 console.log('seer to mon = ', mon);
+
+//2.calculation
+function totalSales(shirtSales, pantSales, shoeSales) {
+    const perShirtPrice = 100;
+    const perPantPrice = 200;
+    const perShoePrice = 500;
+    //sell calculation
+    const totalShirtSales = shirtSales * perShirtPrice;
+    const totalPantSales = pantSales * perPantPrice;
+    const totalShoeSales = shoeSales * perShoePrice;
+
+    //total sales
+    const total = totalShirtSales + totalPantSales + totalShoeSales;
+    return total;
+}
+
+const salesCalculation = totalSales(7, 5, 13);
+console.log('sales Calculation = ', salesCalculation);
